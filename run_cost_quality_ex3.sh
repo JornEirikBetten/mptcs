902ca41b2a188259df8b76ae142901890f20cae8
JornEirikBetten <jorneirikbetten1@gmail.com>
2025-05-12 18:04:02 +0200

    All cost-quality experiments.

--- run_cost_quality_ex3.sh
+++ run_cost_quality_ex3.sh
@@ -3,10 +3,31 @@
 #SBATCH --account=jorneirik 
 #SBATCH --job-name=cost_quality
 #SBATCH --output=cost_quality.log   
-#SBATCH --partition=defq
+#SBATCH --partition=dgx2q
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH -c 6          #  På g001 bør en berense seg til 96 / 16 cores per GPU
 #SBATCH --gres=gpu:1
 
 srun -n 1 python cost-quality.py minatar-asterix 50 20 1
+srun -n 1 python cost-quality.py minatar-asterix 50 20 2
+srun -n 1 python cost-quality.py minatar-asterix 50 20 3
+srun -n 1 python cost-quality.py minatar-asterix 50 20 4
+
+srun -n 1 python cost-quality.py minatar-breakout 50 20 1
+srun -n 1 python cost-quality.py minatar-breakout 50 20 2
+srun -n 1 python cost-quality.py minatar-breakout 50 20 3
+srun -n 1 python cost-quality.py minatar-breakout 50 20 4
+
+srun -n 1 python cost-quality.py minatar-seaquest 50 20 1
+srun -n 1 python cost-quality.py minatar-seaquest 50 20 2
+srun -n 1 python cost-quality.py minatar-seaquest 50 20 3
+srun -n 1 python cost-quality.py minatar-seaquest 50 20 4
+
+srun -n 1 python cost-quality.py minatar-space_invaders 50 20 1
+srun -n 1 python cost-quality.py minatar-space_invaders 50 20 2
+srun -n 1 python cost-quality.py minatar-space_invaders 50 20 3
+srun -n 1 python cost-quality.py minatar-space_invaders 50 20 4
+
+
+
