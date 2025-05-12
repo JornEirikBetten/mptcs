ea1758842cae703250587b563975aae191ea466f
JornEirikBetten <jorneirikbetten1@gmail.com>
2025-05-12 17:57:58 +0200

    Slurm script.

---
+++ run_cost_quality_ex3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --account=jorneirik 
+#SBATCH --job-name=cost_quality
+#SBATCH --output=cost_quality.log   
+#SBATCH --partition=dgx2q
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH -c 6          #  På g001 bør en berense seg til 96 / 16 cores per GPU
+#SBATCH --gres=gpu:1
+
+srun -n 1 python cost-quality.py minatar-asterix 50 20 1
